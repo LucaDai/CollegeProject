c6282489424d92c0b73d611e83f19c6e3fa7d7b3
Luca Dai <fff1996619@gmail.com>
2021-04-11 20:24:16 -0500

    Add files via upload

---
+++ WarEagle.java
@@ -0,0 +1,14 @@
+/**
+War Eagle!!!
+*/
+public class WarEagle {
+
+   public static void main(String[] args) {   
+   
+      System.out.println("War Eagle!");
+      System.out.println("War Eagle!!");
+      System.out.println("War Eagle!!!");
+      
+   }
+
+}
